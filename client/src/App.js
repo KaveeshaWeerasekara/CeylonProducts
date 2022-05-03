157f505340a7641fac8368df73def69e8ff43f35
SunethMadhuwantha <90zuneth99@gmail.com>
2022-05-03 23:17:38 +0530

    sellerRegistrationForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,22 @@
+import "./App.scss";
 
-import './App.scss';
+import Login from "./components/loginAndSignup/pages/Login";
+import Register from "./components/loginAndSignup/pages/Register";
+import Home from "./components/loginAndSignup/pages/Home";
+import Seller from "./components/sellerRegistration/SellerRegistration";
 
-import Login from './components/loginAndSignup/pages/Login';
-import Register from './components/loginAndSignup/pages/Register';
-import Home from './components/loginAndSignup/pages/Home';
-
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
-      
-      
-      <BrowserRouter>
-        <Routes>
-            <Route exact path="/" element={<Home/>}></Route>
-            <Route path="/login" element={<Login/>}></Route>
-            <Route path="/register" element={<Register/>}></Route>
-            
-        </Routes>
-      
-      </BrowserRouter>
-      
-      
-        
-     
-
-      
-    
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/seller" element={<Seller />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
