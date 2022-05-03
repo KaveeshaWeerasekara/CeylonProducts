74c3a319fd47b50a08dd7d75465ee06050c8738b
SunethMadhuwantha <90zuneth99@gmail.com>
2022-05-03 22:19:17 +0530

    add seller registration form

---
+++ seller-registration/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SellerRegistration from "./compoenents/SellerRegistration";
+
+
+function App() {
+  return (
+    <>
+      <SellerRegistration />
+    </>
+  );
+}
+
+export default App;
