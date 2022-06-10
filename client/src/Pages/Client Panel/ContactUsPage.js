24e7ced6073e93b7cf680eb151266968cd101687
Chinthaka-git <kavinduchintaka@gmail.com>
2022-06-10 20:16:10 +0530

    Solve contact us form problems

--- client/src/Pages/Client Panel/ContactUsPage.js
+++ client/src/Pages/Client Panel/ContactUsPage.js
@@ -1,36 +1,24 @@
 import React from "react";
 
-import Nav from '../../components/Client Panel/Nav'
+import Nav from "../../components/Client Panel/Nav";
 import ContactUs from "../../components/Client Panel/contactUs/imports/ContactUs";
 import ContactCeylon from "../../components/Client Panel/contactUs/imports/ContactCeylon";
 
 export default function ContactUsPage(props) {
-
   return (
-    <div className=" ">
+    <div>
       <Nav />
-   
+
+      <div >
       <div className=" grid grid-flow-row grid-cols-2 grid-rows-1">
-     
-           
-         
-         
-            <div className=" col-start-1">
-              <ContactCeylon />
-            
-          </div>
-          <div className=" col-start-2 pl-10">
-              <ContactUs />
-            </div>
-
+        <div className=" col-start-1">
+          <ContactCeylon />
+        </div>
+        <div className=" col-start-2 ">
+          <ContactUs />
+        </div>
       </div>
-
-       
-
-      
+      </div>
     </div>
-
-  )
+  );
 }
-
-
